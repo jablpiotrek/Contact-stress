834f4eae678c15c903311793f400256169b7ecca
Piotr Jablonski <jabl.piotrek@gmail.com>
2018-01-10 15:40:07 +1100

    sttaic background applied

--- src/components/App.js
+++ src/components/App.js
@@ -21,24 +21,16 @@ export default class App extends Component {
             hue: 'hue-rotate(0deg)',
             val: 0
         }; 
-        this.animateBckg= this.animateBckg.bind(this);
+
     }
     
-    animateBckg(i) {
-            let angle = this.state.val + i;
-
-            this.setState({
-                hue: 'hue-rotate(' + angle + 'deg)',
-                val: angle
-            });
-    }
     
     render() {
         return (
             <Provider store = {store}>
                    <Router>
                         <div className = 'app'>
-                            <Header handleClick = {() => {this.animateBckg(90)}}/>
+                            <Header />
                             <Main />
                             <Footer />
                             <div  
--- src/components/Header.js
+++ src/components/Header.js
@@ -14,11 +14,11 @@ export default class  Header extends Component {
                     <h1>Coating Stress Calculator</h1>
                 </div>
                 <nav>
-                    <NavLink onClick = {this.props.handleClick} exact to = '/' activeClassName="active">Home</NavLink>
-                    <NavLink onClick = {this.props.handleClick} to = '/calculate' activeClassName="active">Calculate</NavLink>
-                    <NavLink onClick = {this.props.handleClick} to = '/plot' activeClassName="active">Plot</NavLink>
-                    <NavLink onClick = {this.props.handleClick} to = '/data' activeClassName="active">Data</NavLink>
-                    <NavLink onClick = {this.props.handleClick} to = '/about' activeClassName="active">About</NavLink>
+                    <NavLink exact to = '/' activeClassName="active">Home</NavLink>
+                    <NavLink to = '/calculate' activeClassName="active">Calculate</NavLink>
+                    <NavLink to = '/plot' activeClassName="active">Plot</NavLink>
+                    <NavLink to = '/data' activeClassName="active">Data</NavLink>
+                    <NavLink to = '/about' activeClassName="active">About</NavLink>
                 </nav>
                 
             </div>
