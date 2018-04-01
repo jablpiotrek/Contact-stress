65167cb28276d8453eec55dda7cfd357816faac6
Piotr Jablonski <jabl.piotrek@gmail.com>
2018-04-02 00:03:15 +1000

    format updated - minor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,37 @@
 import React, {Component} from 'react';
 import UNSWLogo from '../images/UNSW.png';
-import { NavLink} from 'react-router-dom';
-
-export default class  Header extends Component {
-    
-    render () {
+import {NavLink} from 'react-router-dom';
+export default class Header extends Component {
+    render() {
         return (
-            <div className = 'header'>
-
-                <div className = 'title'>
-                    <a rel="noopener noreferrer" target = '_blank' href="https://www.unsw.edu.au/"><img src={UNSWLogo} alt="Univeristy of New South Wales Sydney"/></a>
+            <div className='header'>
+                <div className='title'>
+                    <a rel="noopener noreferrer" target='_blank' href="https://www.unsw.edu.au/"><img src={UNSWLogo} alt="Univeristy of New South Wales Sydney"/></a>
                     <h1>Coating Stress Calculator</h1>
                 </div>
                 <nav>
-                    <NavLink exact to = '/' activeClassName="active">
+                    <NavLink exact to='/' activeClassName="active">
                         <i className="fa fa-home" aria-hidden="true"></i>
                         <span>Home</span>
                     </NavLink>
-                    <NavLink to = '/calculate' activeClassName="active">
+                    <NavLink to='/calculate' activeClassName="active">
                         <i className="fa fa-bolt" aria-hidden="true"></i>
                         <span>Calculate</span>
                     </NavLink>
-                    <NavLink to = '/plot' activeClassName="active">
+                    <NavLink to='/plot' activeClassName="active">
                         <i className="fa fa-area-chart" aria-hidden="true"></i>
                         <span>Plot</span>
                     </NavLink>
-                    <NavLink to = '/data' activeClassName="active">
+                    <NavLink to='/data' activeClassName="active">
                         <i className="fa fa-database" aria-hidden="true"></i>
                         <span>Data</span>
                     </NavLink>
-                    <NavLink to = '/about' activeClassName="active">
+                    <NavLink to='/about' activeClassName="active">
                         <i className="fa fa-question-circle-o" aria-hidden="true"></i>
                         <span>About</span>
                     </NavLink>
                 </nav>
-                
             </div>
         );
     }
 }
-
-
